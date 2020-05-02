2f29e79dfe2fb71b2a825b6253b4ff13c757f7a1
djodjo89 <mathysimion@yahoo.fr>
2020-05-02 19:42:02 +0200

    hotfix

--- src/Main.ts
+++ src/Main.ts
@@ -23,10 +23,10 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  // mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
+  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', () => {
